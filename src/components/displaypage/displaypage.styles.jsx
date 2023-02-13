fa74638c6df1329bee7a936801bba1c8d60dc7a0
ltchelishvili1 <tchelishvililevani@gmail.com>
2023-02-13 11:42:48 +0400

    updated scrollbar

--- src/components/displaypage/displaypage.styles.jsx
+++ src/components/displaypage/displaypage.styles.jsx
@@ -4,7 +4,23 @@ export const MainContainer = styled.div`
   height: 850px;
   padding: 48px 80px 44px 75px;
   background-color: white;
-  overflow: scroll;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #bec4c4;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 
   ${({ wholePage }) =>
     wholePage &&
@@ -18,17 +34,15 @@ export const MainContainer = styled.div`
     `}
 `;
 
-
 export const LogoCont = styled.img`
   width: 42px;
   height: 42px;
   position: absolute;
   bottom: 44px;
-`
+`;
 
 export const BackToWelcomeCont = styled.img`
   position: absolute;
   top: 45px;
   left: 48px;
-
-`
+`;
